0e8cf2d15c0686dac8f8eedd1a7a41cfa365b9ee
Andrew <andydjackson@gmail.com>
2012-09-26 21:03:08 +0100

    more

--- commandModule.py
+++ commandModule.py
@@ -1,11 +1,20 @@
 import re
 
+import commands.speak
+
 class CommandModule:
-    def __init__(self):
+    def __init__(self, ircHelper):
         self.regexSplitCommand = re.compile(r"!!(?P<command>\w+)\s(?P<arguments>.*).*")
+        self.commands = dict()
+
+        self.ircHelper = ircHelper
+        
+        commands.speak.create(self.commands)
         
     def runCommand(self, command):
         split = self.regexSplitCommand.match(command)
         commandName = split.group('command')
         commandArguments = split.group('arguments')
+        if self.commands.has_key(commandName):
+            self.commands[commandName](self.ircHelper, commandArguments)
         print commandName + "---" + commandArguments
