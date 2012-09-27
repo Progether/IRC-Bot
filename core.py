50a88b3622504b102e03907ab8935e7fde647610
Kyle Welsh <kywelsh@gmail.com>
2012-09-27 14:26:55 +0200

    Fixed merge issues and added settings module

--- core.py
+++ core.py
@@ -2,14 +2,17 @@ import socket, re
 
 from commandModule import CommandModule
 
-class IRCBot:
-    def __init__(self, network, port, channel, nickname, tempCacheSize=4096):
-        self.network = network
-        self.port = port
-        self.channel = channel
-        self.nickname = nickname
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+from settings import read_config
 
+class IRCBot:
+    def __init__(self, tempCacheSize=4096):
+        conf = read_config()
+        self.network = conf['network']
+        self.port = int(conf['port'])
+        self.channel = conf['channel']
+        self.nickname = conf['nick']
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
         self.tempCacheSize = tempCacheSize
 
         self.commandModule = CommandModule()
@@ -54,4 +57,4 @@ class IRCBot:
             return f
         return decorator
 
-ircBot = IRCBot('irc.freenode.net', 6667, '#progether', 'WorkingIRCBot')
+ircBot = IRCBot()
