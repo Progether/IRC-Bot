edec6c7d8ac8d96d6b210b9d482211c670132904
indivisible-irl <github@indivisibleindustries.com>
2013-11-21 08:19:45 +0000

    major rework of db.py. table creation now handled automatically. methods to handle schema upgrades. error handling and returns added to all methods.

--- db.py
+++ db.py
@@ -5,10 +5,26 @@ import settings
 class DB:
     
     def __init__(self):
+        # info for all tables.  # increment version number when updating create (look at self.__on_upgrade() too)
         self.tables = {
-                       'mail'     : "sender text, recipient text, message text, id text",
-                       'projects' : "name text, language text, link text, description text, id text"
-                       }
+            ##   table     | vers  |  create statement
+                'tables'   : (1,
+                    "table_name text PRIMARY KEY, current_version integer NOT NULL"),
+                'mail'     : (1,
+                    "sender text NOT NULL, recipient text NOT NULL, message text NOT NULL, id text NOT NULL"),
+                'projects' : (1,
+                    "name text NOT NULL, language text NULL, link text NULL, description text NULL, id text NOT NULL"), 
+                'users'    : (1,
+                    "_id integer PRIMARY KEY DEFAULT nextval('serial'), nick text NOT NULL, userlevel integer NOT NULL DEFAULT 0, last_online integer NOT NULL"),
+                'logs'     : (1,
+                    "_id integer PRIMARY KEY DEFAULT nextval('serial'), time type text NOT NULL, log text NOT NULL")
+                }
+        self.__ensure_all_tables_correct()
+
+#future ideas:  
+#  mail: "_id integer PRIMARY KEY DEFAULT nextval('serial'), sender text NOT NULL, recipient text NOT NULL, time_sent integer NOT NULL, message text NOT NULL"
+#  proj: "_id integer PRIMARY KEY DEFAULT nextval('serial'), name text NOT NULL, lang text NULL, owner text NOT NULL, link text NULL, descrip text NOT NULL, status text"
+#  user: possibly include timezone settings to localise timestamps on logs, mail etc for each user (maybe with a '!!me' command group)
         
     def db_connect(self):
         config = settings.read_config()
@@ -21,13 +37,14 @@ class DB:
                             port     = config['db_port'])
             return conn
         except psycopg2.Error as e:
-            print("Error connecting to db")
+            print("!! Error connecting to db")
             print(e)
             return None
     
     def db_add_table(self,table_name,table_info):
         # table_info should follow proper sql format.
         #     i.e: db_add_table("test", "id PRIMARY_KEY, name varchar(20, sample_data text)")
+        print(".. Creating table: %s...\n  Schema: %s" % (table_name, table_info))
         try:
             if not self.db_check_table(table_name):
                 conn = self.db_connect()
@@ -40,7 +57,7 @@ class DB:
                 print("!! Error creating table %s. Already exists" % table_name)
                 return False
         except psycopg2.Error as e:
-            print("Error creating new table: %s" % table_name)
+            print("!! Error creating new table: %s" % table_name)
             print(e)
             return False
         finally:
@@ -62,7 +79,7 @@ class DB:
                 ircHelpers.sayInChannel("There is no table: %s" % table_name)
                 return False
         except psycopg2.Error as e:
-            print("Error dropping table: %s" % table_name)
+            print("!! Error dropping table: %s" % table_name)
             print(e)
             return False
         finally:
@@ -91,7 +108,7 @@ class DB:
                 ircHelpers.sayInChannel("There is no table: %s" % table_name)
                 return False
         except psycopg2.Error as e:
-            print("Error adding data to table: %s" % table_name)
+            print("!! Error adding data to table: %s" % table_name)
             print(e)
             return False
         finally:
@@ -112,7 +129,7 @@ class DB:
             else:
                 return None
         except psycopg2.Error as e:
-            print("Error retrieving data from table: %s" % table_name)
+            print("!! Error retrieving data from table: %s" % table_name)
             print(e)
             return None
         finally:
@@ -133,7 +150,7 @@ class DB:
             else:
                 return None
         except psycopg2.Error as e:
-            print("Error retrieving data from table: %s" % table_name)
+            print("!! Error retrieving data from table: %s" % table_name)
             print(e)
             return None
         finally:
@@ -155,7 +172,7 @@ class DB:
                 ircHelpers.sayInChannel("There is no table: %s" % table_name)
                 return False
         except psycopg2.Error as e:
-            print("Error deleting data from table: %s" % table_name)
+            print("!! Error deleting data from table: %s" % table_name)
             print(e)
             return False
         finally:
@@ -177,7 +194,7 @@ class DB:
                 ircHelpers.sayInChannel("There is no table: %s" % table_name)
                 return False
         except psycopg2.Error as e:
-            print("Error updating data in table: %s" % table_name)
+            print("!! Error updating data in table: %s" % table_name)
             print(e)
             return False
         finally:
@@ -198,7 +215,7 @@ class DB:
                 print("!! DB Table not exists: %s" % table_name)
             return response
         except psycopg2.Error as e:
-            print("Error checking table exists: %s  [ironic, right?]" % table_name)
+            print("!! Error checking table exists: %s  [ironic, right?]" % table_name)
             print(e)
             return False    ##ASK Really want to return False on fail here?? Table may actually exist.
         finally:
@@ -207,11 +224,69 @@ class DB:
             except Exception:
                 pass
 
-    def ensure_all_tables_exist(self):
-        pass
+    def __ensure_all_tables_correct(self):
+        all_tables = self.tables.keys()
+        unimplemented_tables = ('logs', 'users', 'tables')
+        all_successful = True
+        for table in all_tables:
+            if table in unimplemented_tables:
+                continue
+            if self.__ensure_table_correct(table):
+                print(".. Table found: %s" % table)
+            else:
+                print("!! Table not found: %s" % table)
+                if self.__recreate_table(table):
+                    print(".. Created table: %s" % table)
+                else:
+                    print("!! Failed to create table: %s" % table)
+                    all_successful = False
+        return all_successful
     
-    def recreate_table(self, table_name):
-        pass
+    def __ensure_table_correct(self, table_name):
+        # TODO Test old/new version numbers and trigger __on_upgrade, __recreate etc etc
+        #   For now we'll just test if table exists
+        return self.db_check_table(table_name)
+    
+    def __recreate_table(self, table_name):
+        go_for_new = False
+        if self.db_check_table(table_name):
+            if self.db_drop_table(table_name):
+                go_for_new = True
+        else:
+            go_for_new = True
+        
+        if go_for_new:
+            if self.db_add_table(table_name, self.tables[table_name][1]):
+                return True
+        return False
+    
+    
+    def __on_upgrade(self, table_name, new_version, old_version=0):
+        # Can eventually handle upgrades to tables without losing data
+        #     by making use of the version numbers
+        # For now just destroy and recreate the tables
+        # Skipping the projects table to retain info
+        
+        if table_name == 'mail':
+            return self.__recreate_table('mail')
+        
+        elif table_name == 'projects':
+            if self.db_check_table('projects'):
+                print(".. Attempt to upgrade/recreate Projects table. Ignored to preserve info until upgrade possible")
+                return True  # cheaty
+            else:
+                return self.__recreate_table('projects')
+        
+        elif table_name == 'users':
+            return self.__recreate_table('users')
+        
+        elif table_name == 'logs':
+            return self.__recreate_table('logs')
+        
+        else:
+            print("Unsupported table: %s. Add to db.tables and db.__on_upgrade to deploy")
+            return False
+        
         
 if __name__ == "__main__":
     pass
