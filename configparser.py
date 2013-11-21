e848f74456dc201637271eeb8bfcdab8ac51d04f
indivisible-irl <github@indivisibleindustries.com>
2013-11-21 00:55:57 +0000

    more robust fallback for reading settings & added database options to settings

--- configparser.py
+++ configparser.py
@@ -2,17 +2,32 @@ class ConfigParser:
     def __init__(self):
         self.config = dict()
 
-    def read(self, FILE_Name):
-        with open(FILE_Name) as f:
-            content = f.readlines()
-        for item in content:
-            if "[settings]" in item:
-                self.config = { "settings" : dict() }
-            else:
+    def read(self, lines):
+        
+        for line in lines:
+            item = line.strip()
+            if '[settings]' in item:
+                self.config = { "settings" : dict() }       
+            elif '=' in item:
                 items = item.split("=")
                 key = items[0].strip()
                 value = items[1].strip()
                 self.config['settings'][key] = value
+            else:
+                pass
+            
+                
+
+    def read_file(self, settings_file):
+        with open(settings_file) as f:
+            lines = f.readlines()
+            self.read(lines)
+            f.close()
+            
+    def read_raw(self, raw_text):
+        lines = raw_text.splitlines()
+        self.read(lines)
+            
 
     def get(self, first_key, second_key):
         value = self.config[first_key][second_key]
--- settings.py
+++ settings.py
@@ -4,19 +4,34 @@ import os
 current_directory = os.getcwd()
 FILE_NAME = "%s/settings.txt" % current_directory
 
+# default settings (and template for generated file contents)
+file_raw = """  [settings]
+                
+                ## bot user:
+                nick     = progether
+                password = progether_bot
+                
+                ## channel:
+                channel  = #reddit-progether
+                network  = irc.freenode.net
+                port     = 6667
+                
+                ## database
+                db_name = d2k2tmq3q2lk62
+                db_user = ddvzstnjeyvtkk
+                db_pass = qiJbYxnbFTlXBAtRiyRkXGkFub
+                db_host = ec2-23-23-80-55.compute-1.amazonaws.com
+                db_port = 5432
+                
+                ## bot commands
+                quit        = !!quit
+                bot_command = !!
+                """
+
 def create_config():
     config_file = open(FILE_NAME, 'w')
-    file_content = """\
-                    [settings]
-                    nick = tmpircbotname
-                    port = 6667
-                    network = irc.freenode.net
-                    quit = !!quit
-                    channel = #reddit-progether
-                    password = 1234test
-                    bot_command = !!
-                    """
-    file_content = file_content.splitlines()
+    
+    file_content = file_raw.splitlines()
     for line in file_content:
         config_file.write(line.strip() + "\n")
     config_file.close()
@@ -24,18 +39,33 @@ def create_config():
 def read_config():
     config = dict()
     config_file = ConfigParser()
-    try:
-      with open(FILE_NAME):
-        config_file.read(FILE_NAME)
-    except IOError:
-      create_config()
-      config_file.read(FILE_NAME)
-    config['nick'] = config_file.get('settings', 'nick')
-    config['network'] = config_file.get('settings', 'network')
-    config['port'] = config_file.get('settings', 'port')
-    config['channel'] = config_file.get('settings', 'channel')
-    config['quit'] = config_file.get('settings', 'quit')
+    created_new_file = False
+    while True:
+        try:
+            config_file.read_file(FILE_NAME)        # attempt to open existing file
+            break
+        except (IOError, FileNotFoundError) as e:
+            print(e)
+            if not created_new_file:
+                create_config()                     # try to create file if not exists
+            else:
+                config_file.read_raw(file_raw)      # else read direct from default string
+                break
+
+    
+    config['nick']     = config_file.get('settings', 'nick')
     config['password'] = config_file.get('settings', 'password')
+    config['channel']  = config_file.get('settings', 'channel')
+    config['network']  = config_file.get('settings', 'network')
+    config['port']     = config_file.get('settings', 'port')
+    
+    config['db_name'] = config_file.get('settings', 'db_name')
+    config['db_user'] = config_file.get('settings', 'db_user')
+    config['db_pass'] = config_file.get('settings', 'db_pass')
+    config['db_host'] = config_file.get('settings', 'db_host')
+    config['db_port'] = config_file.get('settings', 'db_port')
+    
+    config['quit']        = config_file.get('settings', 'quit')
     config['bot_command'] = config_file.get('settings', 'bot_command')
     return config
 
